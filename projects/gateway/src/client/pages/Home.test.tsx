4fcfc24fcfcbc89d7b12a3ddd944b77e8edd0b15
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2025-05-24 22:29:36 -0600

    add mantine provider to test

--- projects/gateway/src/client/pages/Home.test.tsx
+++ projects/gateway/src/client/pages/Home.test.tsx
@@ -1,10 +1,15 @@
+import { MantineProvider } from '@mantine/core';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { Home } from './Home.tsx';
 
 describe('test Home', () => {
   it('renders', () => {
-    render(<Home />);
+    render(
+      <MantineProvider>
+        <Home />
+      </MantineProvider>,
+    );
     expect(
       screen.getByRole('heading', { name: 'Welcome to Bill Split!' }),
     ).toBeVisible();
