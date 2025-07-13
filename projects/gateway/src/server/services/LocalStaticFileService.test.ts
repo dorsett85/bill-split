0f0a814aaa3a3c43a200803c07782bd4bb1c321d
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2025-07-13 13:09:20 -0600

    update static file service tests

--- projects/gateway/src/server/services/LocalStaticFileService.test.ts
+++ projects/gateway/src/server/services/LocalStaticFileService.test.ts
@@ -7,31 +7,63 @@ beforeEach(() => {
 });
 
 describe('test LocalStaticFileService', () => {
-  it('gets a static file', async () => {
-    // Arrange
-    const expectedString = 'static file contents';
-    const readFileSpy = vi
-      .spyOn(fs, 'readFile')
-      .mockResolvedValue(Buffer.from(expectedString));
-    const staticFileService = new LocalStaticFileService({
-      hostPath: 'host',
-      staticPath: 'static',
-    });
-
-    // Act
-    const buffer = await staticFileService.getAsset('/static-asset.jpg');
-
-    // Assert
-    expect(buffer.toString()).toBe(expectedString);
-    expect(readFileSpy).toHaveBeenCalledOnce();
-    expect(readFileSpy).toHaveBeenCalledWith('host/static-asset.jpg');
-  });
-
   it('creates a new instance without paths', () => {
     const staticFileService = new LocalStaticFileService({
       hostPath: 'host',
       staticPath: 'static',
     });
     expect(staticFileService.has('static/asset.js')).toBe(false);
+  });
+
+  it('gets assets by page', async () => {
+    // Arrange
+    const url = '/';
+    const staticAsset = 'static-asset.jpg';
+    const staticFileManifest = { assetsByPage: { [url]: [staticAsset] } };
+    const readFileSpy = vi
+      .spyOn(fs, 'readFile')
+      .mockResolvedValueOnce(Buffer.from(JSON.stringify(staticFileManifest)));
+    const staticFileService = new LocalStaticFileService({
+      hostPath: 'host',
+      staticPath: 'static',
+    });
+    await staticFileService.populateFilenameCache();
+
+    // Act
+    const assetsByPage = staticFileService.getPageAssetFilenames(url);
+
+    // Assert
+    expect(assetsByPage).toStrictEqual([staticAsset]);
+    expect(readFileSpy).toHaveBeenCalledTimes(1);
+    expect(readFileSpy).toHaveBeenCalledWith(
+      'host/static/staticFileManifest.json',
+    );
+  });
+
+  it('gets a static file', async () => {
+    // Arrange
+    const staticAsset = 'static-asset.jpg';
+    const staticFileManifest = { assetMapping: { [staticAsset]: staticAsset } };
+    const expectedString = 'static file contents';
+    const readFileSpy = vi
+      .spyOn(fs, 'readFile')
+      .mockResolvedValueOnce(Buffer.from(JSON.stringify(staticFileManifest)))
+      .mockResolvedValueOnce(Buffer.from(expectedString));
+    const staticFileService = new LocalStaticFileService({
+      hostPath: 'host',
+      staticPath: 'static',
+    });
+    await staticFileService.populateFilenameCache();
+
+    // Act
+    const buffer = await staticFileService.getAsset(staticAsset);
+
+    // Assert
+    expect(buffer.toString()).toBe(expectedString);
+    expect(readFileSpy).toHaveBeenCalledTimes(2);
+    expect(readFileSpy).toHaveBeenCalledWith(
+      'host/static/staticFileManifest.json',
+    );
+    expect(readFileSpy).toHaveBeenCalledWith('host/static-asset.jpg');
   });
 });
