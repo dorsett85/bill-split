7e4f37e10c36176dc5e81df7f0eb22cbf6898331
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2025-09-21 23:31:16 -0600

    update pct_owes db type

--- projects/db/migrations/00005_create_line_item_participant_table.sql
+++ projects/db/migrations/00005_create_line_item_participant_table.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS line_item_participant (
     id SERIAL PRIMARY KEY,
     line_item_id INT,
     participant_id INT,
-    pct_owes NUMERIC(4, 4),
+    pct_owes REAL,
     FOREIGN KEY (line_item_id) REFERENCES line_item(id) ON DELETE CASCADE,
     FOREIGN KEY (participant_id) REFERENCES participant(id) ON DELETE CASCADE,
     CONSTRAINT line_item_participant_unique_ids UNIQUE(line_item_id, participant_id)
 );
 
-COMMENT ON COLUMN line_item_participant.pct_owes IS 'The percent of the item a participant owes, expressed as a decimal';
+COMMENT ON COLUMN line_item_participant.pct_owes IS 'The percent of the item a participant owes';
