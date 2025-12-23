7a9c0d8a9b12b29e9792aa69c533ed80c839d59b
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2025-12-22 17:41:01 -0700

    add created_at columns

--- projects/db/migrations/00001_create_bill_table.sql
+++ projects/db/migrations/00001_create_bill_table.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS bill (
     image_path VARCHAR,
     image_status VARCHAR CHECK (image_status IN ('parsing', 'ready', 'error')),
     name VARCHAR,
-    tax NUMERIC
+    tax NUMERIC,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMENT ON COLUMN bill.business_name IS 'Name of the business';
@@ -18,3 +19,4 @@ COMMENT ON COLUMN bill.image_path IS 'Storage path of the receipt';
 COMMENT ON COLUMN bill.name IS 'Name of the bill';
 COMMENT ON COLUMN bill.image_status IS 'State of extracting and parsing image text data';
 COMMENT ON COLUMN bill.tax IS 'Tax expressed as a percent';
+COMMENT ON COLUMN bill.created_at IS 'When the record was created';
--- projects/db/migrations/00007_create_access_token_table.sql
+++ projects/db/migrations/00007_create_access_token_table.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS access_token (
   encrypted_token VARCHAR NOT NULL,
   initialization_vector VARCHAR NOT NULL,
   active BOOLEAN NOT NULL,
-  no_of_uses NUMERIC NOT NULL
+  no_of_uses NUMERIC NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMENT ON COLUMN access_token.hashed_token IS 'Hashed access token for lookup';
@@ -12,3 +13,4 @@ COMMENT ON COLUMN access_token.encrypted_token IS 'Encrypted access token';
 COMMENT ON COLUMN access_token.initialization_vector IS 'Initialization vector for the token';
 COMMENT ON COLUMN access_token.active IS 'Whether the token can be used';
 COMMENT ON COLUMN access_token.no_of_uses IS 'How many times users have used the token';
+COMMENT ON COLUMN access_token.created_at IS 'When the record was created';
