03e9b1df5f6256b0a3698b7679dc518da0e1ef86
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2025-12-22 20:59:38 -0700

    lazy instantiate db instance

--- projects/gateway/src/server/db/getDb.ts
+++ projects/gateway/src/server/db/getDb.ts
@@ -3,12 +3,17 @@ import { env } from '../config.ts';
 
 types.setTypeParser(types.builtins.NUMERIC, parseFloat);
 
-const pool = new Pool({
-  database: env.DB_NAME,
-  host: env.DB_HOST,
-  port: Number(env.DB_PORT),
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-});
+let pool: Pool;
 
-export const getDb = (): Pool => pool;
+export const getDb = (): Pool => {
+  if (!pool) {
+    pool = new Pool({
+      database: env.DB_NAME,
+      host: env.DB_HOST,
+      port: Number(env.DB_PORT),
+      user: env.DB_USER,
+      password: env.DB_PASSWORD,
+    });
+  }
+  return pool;
+};
